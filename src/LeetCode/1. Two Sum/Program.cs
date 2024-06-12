1ad69352a1db5a7d34c4495109f9cca55bda51f8
Wael R. Mansour <wael.r.roushdy@gmail.com>
2024-06-11 23:11:40 -0600

    First commit!

---
+++ src/LeetCode/1. Two Sum/Program.cs
@@ -0,0 +1,43 @@
+﻿var sol = new Solution();
+
+sol.TwoSum(new int[] { 2, 7, 11, 15 }, 9).
+	ToList().ForEach(e => Console.WriteLine(e)); // 0,1
+
+sol.TwoSum(new int[] { 3, 2, 4 }, 6).
+	ToList().ForEach(e => Console.WriteLine(e)); // 1,2
+
+sol.TwoSum(new int[] { 3, 3 }, 6).
+	ToList().ForEach(e => Console.WriteLine(e)); // 0,1
+
+sol.TwoSum(new int[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 }, 11).
+	ToList().ForEach(e => Console.WriteLine(e)); // 0,1
+
+Console.ReadKey();
+
+public class Solution
+{
+	public int[] TwoSum(int[] nums, int target)
+	{
+		var result = new int[2];
+
+		var dict = new Dictionary<int, int>();
+		for (int i = 0; i < nums.Length; i++)
+		{
+			if (dict.ContainsKey(nums[i]))
+			{
+				result[0] = dict[nums[i]];
+				result[1] = i;
+			}
+			else
+			{
+				var key = target - nums[i];
+				if (!dict.ContainsKey(key))
+				{
+					dict.Add(key, i);
+				}
+			}
+		}
+
+		return result;
+	}
+}
---
+++ src/LeetCode/217. Contains Duplicate/Program.cs
@@ -0,0 +1,33 @@
+﻿
+var solution = new Solution();
+var arr1 = new int[] { 1, 2, 3, 1 }; // true
+var arr2 = new int[] { 1, 2, 3, 4 }; // false
+var arr3 = new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }; // true
+
+Console.WriteLine(solution.ContainsDuplicate(arr1));
+Console.WriteLine(solution.ContainsDuplicate(arr2));
+Console.WriteLine(solution.ContainsDuplicate(arr3));
+
+Console.ReadKey();
+
+
+public class Solution
+{
+	public bool ContainsDuplicate(int[] nums)
+	{
+		var hash = new HashSet<int>();
+		foreach (int num in nums)
+		{
+			if(!hash.Contains(num))
+			{
+				hash.Add(num);
+			}
+			else
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
