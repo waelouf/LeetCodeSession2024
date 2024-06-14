c64302b623224e31a73b31a57c73aeb55f612673
Wael R. Mansour <wael.r.roushdy@gmail.com>
2024-06-13 22:28:03 -0600

    two more problems!

---
+++ src/LeetCode/347. Top K Frequent Elements/Program.cs
@@ -0,0 +1,52 @@
+﻿var solution = new Solution();
+
+Console.WriteLine(string.Join(",", solution.TopKFrequent([1, 1, 1, 2, 2, 3], 2)));
+Console.WriteLine(string.Join(",", solution.TopKFrequent([1], 1)));
+
+
+public class Solution
+{
+	public int[] TopKFrequent(int[] nums, int k)
+	{
+		var result = new int[k];
+		var dict = new Dictionary<int, int>();
+
+		for (int i = 0; i < nums.Length; i++)
+		{
+			if (!dict.ContainsKey(nums[i]))
+			{
+				dict.Add(nums[i], 0);
+			}
+
+			dict[nums[i]]++;
+		}
+
+		var invertedDictionary = new List<int>[nums.Length + 1];
+        foreach (var key in dict.Keys)
+		{
+			if(invertedDictionary[dict[key]] == null)
+			{
+				invertedDictionary[dict[key]] = new List<int>();
+			}
+
+			invertedDictionary[dict[key]].Add(key);
+		}
+
+		int resultIndex = 0;
+		for (int i = invertedDictionary.Length - 1; i > 0; i--)
+		{
+			if (resultIndex == k) break;
+
+			if (invertedDictionary[i]?.Count > 0)
+			{
+				foreach (var item in invertedDictionary[i])
+				{
+					result[resultIndex++] = item;
+					if (resultIndex == k) break;
+				}
+			}
+		}
+
+		return result; 
+	}
+}
---
+++ src/LeetCode/49. Group Anagrams/Program.cs
@@ -0,0 +1,48 @@
+﻿var solution = new Solution();
+var s1 = solution.GroupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]);
+var s2 = solution.GroupAnagrams([""]);
+var s3 = solution.GroupAnagrams(["a"]);
+
+foreach (var s in s3)
+{
+	foreach (var item in s)
+	{
+		Console.WriteLine(item);
+	}
+
+	Console.WriteLine();
+}
+
+public class Solution
+{
+	public IList<IList<string>> GroupAnagrams(string[] strs)
+	{
+		var indexOfA = (int)'a';
+		var result = new List<IList<string>>();
+		var dict = new Dictionary<string, List<string>>();
+
+		for (int i = 0; i < strs.Length; i++)
+		{
+			var templateArr = new int[26];
+			for (int j = 0; j < strs[i].Length; j++)
+			{
+				templateArr[((int)strs[i][j]) - indexOfA]++;
+			}
+
+			var key = string.Join("_", templateArr);
+			if (!dict.ContainsKey(key))
+			{
+				dict.Add(key, new List<string>());
+			}
+
+			dict[key].Add(strs[i]);
+		}
+
+		foreach (var key in dict.Keys)
+		{
+			result.Add(dict[key]);
+		}
+
+		return result;
+	}
+}
