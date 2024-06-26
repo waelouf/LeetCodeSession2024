6dca2afa9894f263380bbeaba82bd14948b36f1b
Wael R. Mansour <wael.r.roushdy@gmail.com>
2024-06-25 23:20:13 -0600

    bubble sort, two crystal balls problem

---
+++ src/LeetCode/Bubble Sort/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var sol = new BubbleSort();
+int[] arr = [1, 2, 7, 4, 5, 6, 8, 3];
+Console.WriteLine(string.Join(",", arr));
+
+sol.Sort(arr);
+
+Console.WriteLine(string.Join(",", arr));
+
+
+public class BubbleSort
+{
+	public void Sort(int[] arr)
+	{
+		int lastIndex = arr.Length - 1;
+
+		while (lastIndex > 1)
+		{
+			for (int i = 0; i < lastIndex; i++)
+			{
+				if (arr[i] > arr[i+1])
+				{
+					var temp = arr[i];
+					arr[i] = arr[i+1];
+					arr[i+1] = temp;
+				}
+			}
+
+			lastIndex--;
+		}
+	}
+}
---
+++ src/LeetCode/Two Crystal Balls Problem/Program.cs
@@ -0,0 +1,37 @@
+﻿
+bool[] arr = [false, false, false, false, false, false, false, false, false, true];
+
+var solution = new TwoCrystalBalls();
+
+Console.Write(solution.Break(arr));
+public class TwoCrystalBalls
+{
+	public int Break(bool[] breaks)
+	{
+		var increment = (int)Math.Floor(Math.Sqrt(breaks.Length));
+		var searchStartIndex = 0;
+		var currentInex = 0;
+		int breakIndex = -1;
+
+		while (currentInex < breaks.Length)
+		{
+			if (!breaks[currentInex])
+			{
+				searchStartIndex = currentInex;
+				currentInex += increment;
+			}
+			else
+			{
+				for (int i = searchStartIndex; i <= currentInex; i++)
+				{
+					if (breaks[i])
+					{
+						return i;
+					}
+				}
+			}
+		}
+
+		return breakIndex;
+	}
+}
