f66d6ca39aa3bfc0d8817514713856b65010f111
Wael R. Mansour <wael.r.roushdy@gmail.com>
2024-07-07 17:46:22 -0600

    queue and stack implementation

--- src/LeetCode/Linked List/Program.cs
+++ src/LeetCode/Linked List/Program.cs
@@ -1,45 +1,106 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
 
-var queue = new Queue<string>();
-queue.Enqueue("A");
-queue.Enqueue("B");
-queue.Enqueue("C");
+//var queue = new Queue<string>();
+//queue.Enqueue("A");
+//queue.Enqueue("B");
+//queue.Enqueue("C");
 
-Console.WriteLine(queue.Peek());
-Console.WriteLine(queue.Dequeue());
-Console.WriteLine(queue.Peek());
-Console.WriteLine(queue.Count);
-Console.WriteLine(queue.Dequeue());
+//Console.WriteLine(queue.Peek());
+//Console.WriteLine(queue.Deque());
+//Console.WriteLine(queue.Peek());
+//Console.WriteLine(queue.Count);
 
+//Console.WriteLine(queue.Deque());
+
+
+var stack = new Stack<int>();
+stack.Push(1);
+stack.Push(2);
+Console.WriteLine(stack.Peek());
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+
+Console.ReadLine();
+
+public class Stack<T>
+{
+	private StackNode<T> head;
+	private int count;
+    public Stack()
+    {
+		head = null;
+		count = 0;
+    }
+
+    public void Push(T item)
+	{
+		var node = new StackNode<T>(item);
+		count++;
+
+		if (head == null)
+		{
+			head = node;
+			return;
+		}
+
+		node.Previous = head;
+		head = node;
+		
+	}
+
+	public T Pop()
+	{
+		var temp = head;
+
+		head = head.Previous;
+		count--;
+		return temp.Value;
+	}
+
+	public T Peek()
+	{
+		return head.Value;
+	}
+}
+
+public class StackNode<T>
+{
+    public T Value { get; set; }
+
+    public StackNode<T> Previous { get; set; }
+
+    public StackNode(T val)
+    {
+		this.Value = val;
+		this.Previous = null;
+    }
+}
 
 public class Queue<T>
 {
-	private Node<T> head;
-	private Node<T> tail;
-	private int _count;
+	private Node<T>? head;
+	private Node<T>? tail;
+	private int count;
 
-	public int Count
-	{
-		get
-		{
-			return _count;
-		}
-	}
+	public int Count => count;
 
-    public Queue()
+	public Queue()
     {
-		_count = 0;
+		head = null;
+		tail = null;
+		count = 0;
     }
 
-    public void Enqueue(T value)
+	public void Enqueue(T item)
 	{
-		var node = new Node<T>(value);
-		if (tail != null)
+		var node = new Node<T>(item);
+		if (tail == null)
 		{
-			tail.Next = node;
+			tail = node;
 		}
 
+		tail.Next = node;
 		tail = node;
 
 		if(head == null)
@@ -47,23 +108,24 @@ public class Queue<T>
 			head = tail;
 		}
 
-		_count++;
+		count++;
 	}
 
-	public T Dequeue()
+	public T Deque()
 	{
-		if (Count == 0) throw new IndexOutOfRangeException();
-		var val = head.Value;
-		head = head.Next;
+		if (count == 0) throw new NullReferenceException();
+		var temp = head;
+		head = temp.Next;
 
-		_count--;
-		return val;
+		count--;
+		return head.Value;
 	}
 
 	public T Peek()
 	{
 		return head.Value;
 	}
+
 }
 
 public class LinkedList<T>
