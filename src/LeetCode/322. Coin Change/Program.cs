a971350cd817550ea4db3ea9e3f55c3375845738
Wael R. Mansour <wael.r.roushdy@gmail.com>
2024-07-22 22:45:30 -0600

    some more leetcode

---
+++ src/LeetCode/322. Coin Change/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
+
+var sol = new Solution();
+Console.WriteLine(sol.CoinChange([1, 2, 5], 11));
+Console.WriteLine(sol.CoinChange([2], 3));
+Console.WriteLine(sol.CoinChange([1], 0));
+
+public class Solution
+{
+	public int CoinChange(int[] coins, int amount)
+	{
+		var lst = coins.ToList();
+		lst.Sort();
+
+		int count = 0;
+
+
+
+		return count;
+	}
+}
---
+++ src/LeetCode/647. Palindromic Substrings/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var sol = new Solution();
+
+Console.WriteLine(sol.CountSubstrings("abc")); // 3
+Console.WriteLine(sol.CountSubstrings("aaa")); // 6
+Console.WriteLine(sol.CountSubstrings("a")); // 1
+Console.WriteLine(sol.CountSubstrings("")); // 0
+Console.WriteLine(sol.CountSubstrings("aba")); // 0
+
+
+public class Solution
+{
+	public int CountSubstrings(string s)
+	{
+		if (s.Length <= 1) return s.Length ;
+		int count = 0;
+
+		for (int i = 0; i < s.Length; i++)
+		{
+			countPalendrome(i, i);
+
+			countPalendrome(i, i+1);
+		}
+
+		void countPalendrome(int left, int right)
+		{
+			while (left >= 0 && right < s.Length && s[left] == s[right])
+			{
+				count++;
+				left--;
+				right++;
+			}
+		}
+
+		return count;
+	}
+}
