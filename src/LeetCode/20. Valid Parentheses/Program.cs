3568d5cf5a39c9a6c64534769254c5d086134a5b
Wael R. Mansour <wael.r.roushdy@gmail.com>
2024-07-25 21:45:54 -0600

    couple of stacks

---
+++ src/LeetCode/20. Valid Parentheses/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+
+Console.WriteLine("Hello, World!");
+
+var sol = new Solution();
+Console.WriteLine(sol.IsValid("()"));
+Console.WriteLine(sol.IsValid("()[]{}"));
+Console.WriteLine(sol.IsValid("(]"));
+Console.WriteLine(sol.IsValid(""));
+Console.WriteLine(sol.IsValid("(("));
+Console.WriteLine(sol.IsValid("([{}])"));
+
+public class Solution
+{
+	public bool IsValid(string s)
+	{
+		if(s.Length==0) return false;
+		if (s.Length % 2 == 1) return false;
+
+		var openSet = new Dictionary<char, char>
+		{
+			{ '(', ')' },
+			{ '[', ']' },
+			{ '{', '}' }
+		};
+
+
+		var stack = new Stack<char>();
+
+		for (int i = 0; i < s.Length; i++)
+		{
+			if (openSet.ContainsKey(s[i]))
+			{
+				stack.Push(s[i]);
+			}
+			else
+			{
+				if (stack.Count == 0) return false;
+				var openParen = stack.Pop();
+				if(s[i] != openSet[openParen]) return false;
+			}
+		}
+
+		if (stack.Count > 0) return false;
+
+		return true;
+	}
+}
---
+++ src/LeetCode/739. Daily Temperatures/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var sol = new Solution();
+Console.WriteLine(string.Join(",", sol.DailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))); // [1,1,4,2,1,1,0,0]
+Console.WriteLine(string.Join(",", sol.DailyTemperatures([30, 40, 50, 60])));
+Console.WriteLine(string.Join(",", sol.DailyTemperatures([30, 60, 90])));
+
+
+public class Solution
+{
+	public int[] DailyTemperatures(int[] temperatures)
+	{
+		if (temperatures.Length == 0) return new int[0];
+		if (temperatures.Length == 1) return [0];
+
+		var result = new int[temperatures.Length];
+
+		var stack = new Stack<(int, int)>();
+		stack.Push((temperatures[0], 0));
+
+		for (int i = 1; i < temperatures.Length; i++)
+		{
+			while (stack.Count > 0 && temperatures[i] > stack.Peek().Item1)
+			{
+				var temp = stack.Pop();
+				result[temp.Item2] = i - temp.Item2;
+			}
+
+			stack.Push((temperatures[i], i));
+		}
+		
+
+		return result;
+	}
+}
