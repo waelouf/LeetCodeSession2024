8c75442a77eecfc4046842856731168b2add14b4
Wael R. Mansour <wael.r.roushdy@gmail.com>
2024-08-03 13:12:47 -0700

    solve a couple of binary search

---
+++ src/LeetCode/704. Binary Search/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var sol = new Solution();
+ Console.WriteLine(sol.Search([-1, 0, 3, 5, 9, 12], 9));
+ Console.WriteLine(sol.Search([-1, 0, 3, 5, 9, 12], 2));
+Console.WriteLine(sol.Search([5], 5));
+Console.WriteLine(sol.Search([2, 5], 5));
+
+
+public class Solution
+{
+	public int Search(int[] nums, int target)
+	{
+		var foundIndex = -1;
+		
+		int start = 0;
+		int end = nums.Length - 1;
+
+		while (start <= end)
+		{
+			var middleIndex = (end + start) / 2;
+			if (nums[middleIndex] == target)
+			{
+				return middleIndex;
+			}
+
+			if (nums[middleIndex] > target)
+			{
+				end = middleIndex - 1;
+			}
+			else
+			{
+				start = middleIndex + 1;
+			}
+			
+		}
+
+		return foundIndex;
+	}
+}
---
+++ src/LeetCode/74. Search a 2D Matrix/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var sol = new Solution();
+Console.WriteLine(sol.SearchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13));
+Console.WriteLine(sol.SearchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3));
+Console.WriteLine(sol.SearchMatrix([[1, 3]], 3));
+
+public class Solution
+{
+	public bool SearchMatrix(int[][] matrix, int target)
+	{
+		bool found = false;
+
+		var top = 0;
+		var btm = matrix.Length - 1;
+
+		while (top <= btm)
+		{
+			var middle = (top + btm) / 2;
+			var row = matrix[middle];
+			if (row[0] > target)
+			{
+				btm = middle - 1;
+			}
+			else if (row[row.Length - 1] < target)
+			{
+				top = middle + 1;
+			}
+			else
+				break;
+		}
+
+
+		BinarySearch(matrix[(top + btm) / 2]);
+
+
+
+
+		void BinarySearch(int[] lst)
+		{
+			int left = 0;
+			int right = lst.Length - 1;
+			while (left <= right)
+			{
+				int middle = (left + right) / 2;
+				if (lst[middle] == target)
+				{
+					found = true;
+					break;
+				}
+				else if (lst[middle] > target)
+				{
+					right = middle - 1;
+				}
+				else
+				{
+					left = middle + 1;
+				}
+			}
+		}
+
+		return found;
+	}
+}
